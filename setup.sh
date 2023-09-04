35aa5f2e46a4072be2c19264236a05805a87917e
Gibson <>
2023-09-04 22:34:36 +0300

    setup - file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-python main.py CS5_CCN_VOICE_MA 2023-04-01 2023-05-01
-
-
-
+: '
+$1 - file name
+$2 - start_date
+$3 - end_date
+$4 - concurency
+$5 - number of threads
+'
+python main.py CS5_CCN_VOICE_MA 2023-04-01 2023-06-01 1 2
